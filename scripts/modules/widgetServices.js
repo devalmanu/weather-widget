79da2f08fa40d1da66c60276102f52b34d442c31
devalmanu <manuilova-1986@mail.ru>
2023-06-25 16:46:31 +0300

    feat IP location

--- scripts/modules/widgetServices.js
+++ scripts/modules/widgetServices.js
@@ -5,6 +5,14 @@ import {
     renderWidgetForecast } from "./render.js";
 
 export const startWidget = async (city, widget) => {
+    /* проверка города */
+    if (!city) {
+        const dataCity = await getCity();
+        if(dataCity.success) {
+            city = dataCity;
+        }
+    }
+    
     if (!widget) {
         widget = document.createElement('div');
         widget.classList.add('widget');
--- scripts/widget.js
+++ scripts/widget.js
@@ -2,8 +2,7 @@ import { cityServicesSearch } from './modules/cityServicesSearch.js';
 import { startWidget } from './modules/widgetServices.js';
 
 const initWidget = async (app) => {
-    const city = "Минск";
-
+    // const city = "Минск";
     const widget = await startWidget(city);
     app.append(widget);
 
